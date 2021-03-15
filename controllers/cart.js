c5e89b67830442799f963d4d531edd9341b39ca2
vrindajindal2003 <vrindajindal2003@gmail.com>
2021-03-15 16:16:31 +0530

    post methods added

--- controllers/cart.js
+++ controllers/cart.js
@@ -20,16 +20,72 @@ exports.get_test = (req,res,next) => {
 
 };
 
-exports.post_test = (req,res,next) => {
-    // const title = req.body.title;
-    // const image = req.body.image
-    // const price = req.body.price;
-    // const quantity = req.body.quantity;
-    // const product = new Prod( title, image, price,quantity);
-    // product
-    //     .add_prod()
-    //     .then(() => {
-    //         res.redirect('/cart');
-    //     })
-    //     .catch(err => console.log(err));
+exports.post_test = (req,res_main,next) => {
+    console.log("buy button pressed");
+    var credit;
+    const q1= 'select credit from users';
+    const q2 = 'select coalesce(sum(c.quantity*p.price), 0) from cart c, products p where c.item_id = p.id';
+    
+    pool.query(q1, (err, credit_res) => {
+        if(err) {
+            console.log(err);
+            return;
+        }
+        pool.query(q2 , (err, price_res) => {
+            if(err) {
+                console.log(err);
+                return;
+            }
+            // console.log(price_res.rows[0].coalesce);
+            if(price_res.rows[0].coalesce > credit_res.rows[0].credit) {
+                res_main.redirect("/cart");
+            }
+            const q3 = 'insert into orders(user_id, item_id, quantity) select c.user_id, c.item_id, c.quantity from cart c on conflict (user_id, item_id) do update set quantity = (orders.quantity + (select c2.quantity from cart c2 where (orders.user_id, orders.item_id) = (c2.user_id, c2.item_id)));';
+            const q4 = 'delete from cart';
+            const q5 = 'update users set credit = $1 where user_id =1 ;';
+            const dec = credit_res.rows[0].credit - price_res.rows[0].coalesce;
+            console.log("diff: ", dec);
+
+            pool.query('BEGIN', err => {
+                if(err) {
+                    console.log("c1");
+                    return;
+                }
+                pool.query(q3, (err, res) => {
+                    if(err) {
+                        console.log("c2");
+                        return;
+                    }
+                    pool.query(q4, (err, res) => {
+                        if(err) {
+                            console.log("c3");
+                            return;
+                        }
+                        pool.query(q5, [dec], (err, res) => {
+                            if(err) {
+                                console.log("c4");
+                                return;
+                            }
+                            pool.query('COMMIT', err => {
+                                if(err){
+                                    console.log("c5");
+                                    return;
+                                }
+                                else{
+                                    console.log("c6");
+                                    res_main.redirect("/orders");
+                                }
+                            })
+                        })
+
+                    })
+                })
+            })
+
+        });
+
+    });
 };
+ 
+
+    
--- controllers/prods.js
+++ controllers/prods.js
@@ -14,16 +14,59 @@ exports.get_test = (req,res,next) => {
     });
 };
 
-exports.post_test = (req,res,next) => {
-    // const title = req.body.title;
-    // const image = req.body.image
-    // const price = req.body.price;
-    // const quantity = req.body.quantity;
-    // const product = new Prod( title, image, price,quantity);
-    // product
-    //     .add_prod()
-    //     .then(() => {
-    //         res.redirect('/cart');
-    //     })
-    //     .catch(err => console.log(err));
+exports.post_test = (req,res_main,next) => {
+    const prod_id = req.body.product_id;
+    console.log("here",req.body.product_id);
+    pool.query('select * from products where id = $1 and quantity > 0', [prod_id], (err, res) =>{
+        if(err) { 
+            console.log("error", err);
+        }
+        else{
+            console.log("no error: ", res.rows.length);
+            if (res.rows.length == 0) { // item-inventory zero
+                res_main.redirect("/prods");
+            }
+            if (res.rows.length != 0) { // item-inventory not zero
+                const q1 = 'insert into cart(user_id, item_id, quantity) values(1, $1, 1) on conflict (user_id, item_id) do update set quantity = cart.quantity+1';
+                const q2 = 'update products set quantity = (products.quantity -1) where id = $1';
+
+                pool.query('BEGIN', // begin atomic transaction
+         err => {
+             if (err) {
+                 console.log("1");
+                 return;
+             }
+             pool.query(q1, [prod_id], (err, res) =>{ // add to cart form user id = 1
+                 if (err) {
+                    console.log("2");
+                     return;
+                 }
+                 pool.query(q2,[prod_id], (err, res) => { // 
+                     if(err) {
+                        console.log("3");
+                         return;
+                     }
+
+                     pool.query('COMMIT', err => {
+                         if(err) {
+                             console.log("4");
+                             console.log(err);
+                         }
+                         else{
+                            console.log("5");
+                            res_main.redirect('/cart');
+                         }
+                     });
+                 });
+             });
+        
+     }); 
+
+            }
+            
+        }
+    })
 };
+    
+    
+   
